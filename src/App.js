b03676a129711428871a16e22a9a20237a015584
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-05-12 12:51:05 -0600

    event handling

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ class App extends Component {
     this.state = {
       worldData: [],
     }
+
+    this.handleCountryClick = this.handleCountryClick.bind(this);
   }
 
   projection() {
     return geoMercator()
       .scale(100)
       .translate([ 800 / 2, 450 / 2 ]);
+  }
+
+  handleCountryClick(countryIndex) {
+    console.log("clicked on a country: ", this.state.worldData[countryIndex])
   }
 
   componentDidMount() {
@@ -55,26 +61,27 @@ class App extends Component {
             Spaceship
           </div>
         </header>
-        <svg width={ 800 } height={ 450 } viewBox="0 0 800 450">
+        <svg width={800} height={450} viewBox="0 0 800 450">
           <g className="countries">
             {
               this.state.worldData.map((d, i) => (
                 <path
-                  key={ `path-${ i }` }
-                  d={ geoPath().projection(this.projection())(d) }
+                  key={`path-${i}`}
+                  d={geoPath().projection(this.projection())(d)}
                   className="country"
-                  fill={ `rgba(38,50,56,${1 / this.state.worldData.length * i})` }
+                  fill={`rgba(38,50,56,${1 / this.state.worldData.length * i})`}
                   stroke="#FFFFFF"
-                  strokeWidth={ 0.5 }
+                  strokeWidth={0.5}
+                  onClick={() => this.handleCountryClick(i) }
                 />
               ))
             }
           </g>
           <g className="markers">
             <circle
-              cx={ this.projection()([8,48])[0] }
-              cy={ this.projection()([8,48])[1] }
-              r={ 10 }
+              cx={this.projection()([8,48])[0]}
+              cy={this.projection()([8,48])[1]}
+              r={10}
               fill="#E91E63"
               className="marker"
             />
