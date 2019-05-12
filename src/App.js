245fd56383936b5e8ecf08f365249a319d820f88
Clare Politano Hutchings <cpolitano8@gmail.com>
2019-05-12 12:35:10 -0600

    react svg map with topojson d3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
-import L from 'leaflet';
+// import L from 'leaflet';
+import { geoMercator, geoPath } from "d3-geo";
+import { feature } from "topojson-client";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      worldData: [],
+    }
+  }
+
+  projection() {
+    return geoMercator()
+      .scale(100)
+      .translate([ 800 / 2, 450 / 2 ]);
+  }
 
   componentDidMount() {
-    this.map = L.map("mapdiv", {
-      center: [19.4, -99.2],
-      zoom: 13,
-      layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
-      ]
+    // this.map = L.map("mapdiv", {
+    //   center: [19.4, -99.2],
+    //   zoom: 13,
+    //   layers: [
+    //     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //     }),
+    //   ]
+    // });
+
+    fetch("/world-110m.json", {
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then((res) => res.json())
+    .then((worldData) => {
+      console.log(worldData);
+      this.setState({
+        worldData: feature(worldData, worldData.objects.countries).features
+      });
+    },
+    (err) => {
+      console.log(`There was a problem: ${err}`);
     });
   }
 
@@ -24,6 +55,31 @@ class App extends Component {
             Spaceship
           </div>
         </header>
+        <svg width={ 800 } height={ 450 } viewBox="0 0 800 450">
+          <g className="countries">
+            {
+              this.state.worldData.map((d, i) => (
+                <path
+                  key={ `path-${ i }` }
+                  d={ geoPath().projection(this.projection())(d) }
+                  className="country"
+                  fill={ `rgba(38,50,56,${1 / this.state.worldData.length * i})` }
+                  stroke="#FFFFFF"
+                  strokeWidth={ 0.5 }
+                />
+              ))
+            }
+          </g>
+          <g className="markers">
+            <circle
+              cx={ this.projection()([8,48])[0] }
+              cy={ this.projection()([8,48])[1] }
+              r={ 10 }
+              fill="#E91E63"
+              className="marker"
+            />
+          </g>
+        </svg>
         <div id="mapdiv" style={{height:"500px"}}></div>
       </div>
     );
